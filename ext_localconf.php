8541e40c69cff81eb498978a9f8d696b7c4f8d28
Franz Holzinger <holzinger@ttproducts.de>
2017-08-24 17:26:55 +0200

    * replace the TABs by blank characters * add a composer.json file

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) {
-	die ('Access denied.');
+    die ('Access denied.');
 }
 
 define('EXPORT_EXT', $_EXTKEY);
@@ -8,31 +8,31 @@ define('EXPORT_CSHKEY', '_MOD_system_txschedulerM1_' . $_EXTKEY); // key for the
 
 // Add the export task
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['JambageCom\\Export\\Task\\ExportTask'] = array(
-	'extension' => $_EXTKEY,
-	'title' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xlf:exportTask.name',
-	'description' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xlf:exportTask.description',
-	'additionalFields' => 'JambageCom\\Export\\Task\\ExportTaskAdditionalFieldProvider'
+    'extension' => $_EXTKEY,
+    'title' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xlf:exportTask.name',
+    'description' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xlf:exportTask.description',
+    'additionalFields' => 'JambageCom\\Export\\Task\\ExportTaskAdditionalFieldProvider'
 );
 
 $_EXTCONF = unserialize($_EXTCONF);    // unserializing the configuration so we can use it here:
 
 if (
-	isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]) &&
-	is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY])
+    isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]) &&
+    is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY])
 ) {
-	$tmpArray = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY];
+    $tmpArray = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY];
 } else if (isset($tmpArray)) {
-	unset($tmpArray);
+    unset($tmpArray);
 }
 
 if (isset($_EXTCONF) && is_array($_EXTCONF)) {
-	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = $_EXTCONF;
-	if (isset($tmpArray) && is_array($tmpArray)) {
-		$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] =
-			array_merge($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY], $tmpArray);
-	}
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = $_EXTCONF;
+    if (isset($tmpArray) && is_array($tmpArray)) {
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] =
+            array_merge($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY], $tmpArray);
+    }
 } else if (!isset($tmpArray)) {
-	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = array();
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = array();
 }
 
 
--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
 // \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(EXPORT_EXT, 'Configuration/TypoScript', 'Export Task');
@@ -8,10 +8,10 @@ if (!defined('TYPO3_MODE')) {
 
 if (TYPO3_MODE === 'BE') {
 
-	// Add context sensitive help (csh) to the backend module
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-		EXPORT_CSHKEY,
-		'EXT:' . EXPORT_EXT . '/Resources/Private/Language/locallang_csh_export.xlf'
-	);
+    // Add context sensitive help (csh) to the backend module
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+        EXPORT_CSHKEY,
+        'EXT:' . EXPORT_EXT . '/Resources/Private/Language/locallang_csh_export.xlf'
+    );
 }
 
